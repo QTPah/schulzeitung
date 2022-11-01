ae659e14d8d7a0d28d69ef4fbf60df84d7b8691c
QTPah <simonas@zabulionis.net>
2022-11-01 19:36:12 +0100

    server update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
+const express = require('express');
+const path = require('path');
 
-const path = require("path");
-const express = require("express");
-const app = express(); // create express app
+const app = express();
 
-// add middlewares
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
-app.use(express.static("public"));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-// start express server on port 5000
-app.listen(5001, () => {
-  console.log("server started on port 5001");
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
 });
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
