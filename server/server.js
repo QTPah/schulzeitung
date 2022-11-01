0105b821c949866fb405028a4cfa54cd2e51d15d
QTPah <simonas@zabulionis.net>
2022-11-01 19:42:54 +0100

    update

--- server/server.js
+++ server/server.js
@@ -4,14 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
